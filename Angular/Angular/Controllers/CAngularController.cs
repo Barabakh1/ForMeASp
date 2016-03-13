d3472e5cc680514006779992230e8d5acc5f6089
Yura Barabakh <yura.barabakh@gmail.com>
2016-03-13 14:08:41 +0400

    Create new Branch

---
+++ Angular/Angular/Controllers/CAngularController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Angular.Controllers
+{
+    public class CAngularController : Controller
+    {
+        //
+        // GET: /CAngular/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
